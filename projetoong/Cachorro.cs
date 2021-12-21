cb2d05c748e2a628520da0ea1548090b06136b11
Lays <layscarolinam@gmail.com>
2021-12-21 16:30:24 -0300

    Atividade ong pata Amiga

---
+++ projetoong/Cachorro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoong {
+    class Cachorro {
+
+
+        public string nome { get; set; }
+        public string? doenca { get; set; }
+
+
+        public void Checkup() {
+            Console.WriteLine("Foi encontrando doenças no cachorro ?");
+            bool decision = bool.Parse(Console.ReadLine());
+            if (decision) this.doenca = Console.ReadLine();
+
+        }
+    }
+}
---
+++ projetoong/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace projetoong {
+    class Program {
+        static void Main(string[] args) {
+
+            Cachorro[] x = new Cachorro[10];
+
+          for(int i = 0; i < 10; i+= 1) {
+                x[i] = new Cachorro();
+                x[i].nome = Console.ReadLine();
+                x[i].doenca = null;
+            }
+
+          foreach(Cachorro y in x) {
+                y.Checkup();
+            }
+        }
+    }
+}
